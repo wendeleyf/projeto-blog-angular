0675d8cba79298eae4f49ab7e9e1d60795afdaff
Wendeley Fernandes <wendeleyf@gmail.com>
2020-02-13 14:27:18 -0300

    Add form ao form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
@@ -20,7 +20,8 @@ import { PostSecundarioComponent } from './home/post-secundario/post-secundario.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
