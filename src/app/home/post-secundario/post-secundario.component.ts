a9facd21aabe4b0fd1b51a45eb7ac6e400207555
Wendeley Fernandes <wendeleyf@gmail.com>
2020-02-13 13:49:13 -0300

    Update post-secundario component

--- src/app/home/post-secundario/post-secundario.component.ts
+++ src/app/home/post-secundario/post-secundario.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post-secundario',
   templateUrl: './post-secundario.component.html',
-  styleUrls: ['./post-secundario.component.css']
 })
 export class PostSecundarioComponent implements OnInit {
 
