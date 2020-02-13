50350c5a4c2c310b70d364047610236d47dfcf86
Wendeley Fernandes <wendeleyf@gmail.com>
2020-02-13 15:57:31 -0300

    Inserindo o post no localStorage

--- src/app/services/post-service.ts
+++ src/app/services/post-service.ts
@@ -7,6 +7,13 @@ export class PostService {
   constructor() { }
 
   salvar(post: Post){
-    console.log(post);
+    const posts = this.buscarTodos();
+    posts.push(post);
+    window.localStorage.setItem('posts', JSON.stringify(posts));
+    // console.log(post);
+  }
+
+  buscarTodos(): Post[] {
+    return JSON.parse(window.localStorage.getItem('posts')) || [];
   }
 }
